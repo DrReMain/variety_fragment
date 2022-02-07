ece286ef7a5b10eed77a7a12f1e1170e7191e5e0
shijiao <51071534@qq.com>
2022-02-07 17:37:20 +0800

    debug

--- main.c
+++ main.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
 
+// #define debug(M, ...) fprintf(stderr, "DEBUG %s:%d: " M "\n", __FILE__, __LINE__, ##__VA_ARGS__)
+#define log(M, ...) printf("%s:%d: " M "\n", __FILE__, __LINE__, ##__VA_ARGS__)
+
 int main(int argc, char const *argv[])
 {
-    printf("Hello World\n");
+    log("abc");
     return 0;
 }
