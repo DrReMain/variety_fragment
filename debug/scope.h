bcf5b92f56519f073971b17bad100baf74c609b8
shijiao <shijiao@ubuntu.cyber.tech>
2022-02-14 23:43:21 +0800

    io

---
+++ debug/scope.h
@@ -0,0 +1,13 @@
+#ifndef __SCOPE_H__
+#define __SCOPE_H__
+
+extern int THE_SIZE;
+
+int get_age();
+void set_age(int age);
+
+double update_ratio(double ratio);
+
+void print_size();
+
+#endif
--- main.c
+++ main.c
@@ -1,19 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <time.h>
 
 int main(int argc, char const *argv[])
 {
-    time_t t = time(NULL);
+    // time_t t = time(NULL);
+    // srand(t);
+    // for (int i = 0; i < 2; i++)
+    // {
+    //     printf("rand(): %d\n", rand() % 10 + 1);
+    // }
 
-    printf("%lu\n", t);
+    char str[1000] = {'a'};
+    memset(str, 'x', 1000);
 
-    srand(t);
-
-    for (int i = 0; i < 4; i++)
-    {
-        printf("rand(): %d\n", rand() % 10 + 1);
-    }
+    printf("%s\n", str);
 
     return 0;
 }
